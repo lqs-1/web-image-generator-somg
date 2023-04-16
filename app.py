dcab16f27681279507e626d4d331f27c809b6a54
lqs-1 <749062870@qq.com>
2023-04-16 13:48:16 +0800

    finish-0.4

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
+import time
+from logging.handlers import RotatingFileHandler
 import openai
 from flask import Flask, request, jsonify
 from flask_cors import cross_origin
-
 import datetime
-
 import requests
 from bs4 import BeautifulSoup
 import logging
 
-logging.getLogger()
+# 配置日志信息
+# 设置日志记录等级
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+# 配置日志记录器,指明日志保存的路径,每个日志文件的最大大小,保存的日志文件的个数上限
+file_log_error_handler = RotatingFileHandler(f"logs/chat/error_{time.strftime('%Y-%m-%d')}.log", maxBytes=1024*1024*100, backupCount=10)
+# file_log_all_handler = RotatingFileHandler(f"logs/all_{time.strftime('%Y-%m-%d')}.log", maxBytes=1024*1024*100, backupCount=10)
+# 配置写入等级
+file_log_error_handler.setLevel(logging.ERROR)
+# file_log_all_handler.setLevel(logging.INFO)
+# 创建日志记录格式
+formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+# 为刚创建的日志记录器设置日志记录格式
+file_log_error_handler.setFormatter(formatter)
+# file_log_all_handler.setFormatter(formatter)
+logger.addHandler(file_log_error_handler)
+# logger.addHandler(file_log_all_handler)
 
 
 app = Flask(__name__)
@@ -21,6 +37,9 @@ openai.api_key = "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"
 def index():
     # 获取会话的问题数据
     data = request.get_json()
+
+    app.logger.info(str(datetime.datetime.now()) + "======> 提问 =====> " + data.get("request"))
+
     # 请求chartGPT
     try:
 
