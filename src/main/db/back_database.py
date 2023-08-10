9a7f7856b1aa1d63bd59fd6ef14567eafeb39a06
lqs-1 <88884239+lqs-1@users.noreply.github.com>
2023-08-10 12:12:28 +0800

    fi

--- src/main/db/back_database.py
+++ src/main/db/back_database.py
@@ -23,43 +23,42 @@ MYSQL_DICT_USERNAME = 'somg'
 MYSQL_DICT_PASSWORD = 'somg'
 MYSQL_DICT_DB = 'storage'
 
-class DatabaseBR:
 
+class DatabaseBR:
     # 通用配置 要备份的配置 先从配置的字典数据库中获取 没有在按这里的默认配置
     # 数据库主机地址
-    MYSQL_HOST = '192.168.153.128'
+    MYSQL_HOST = None
 
     # MySQL数据库用户名
-    MYSQL_USERNAME = 'root'
+    MYSQL_USERNAME = None
 
     # 数据库密码
-    MYQSL_PASSWORD = '123456'
+    MYQSL_PASSWORD = None
 
     # 数据库端口
-    MYSQL_PORT = 3306
+    MYSQL_PORT = None
 
     # 排除，不进行备份操作的数据库名称集合
     IGNORE_DATABASES = set()
 
     # 备份文件存放路径
-    BACKUP_PATH = ''
+    BACKUP_PATH = None
 
     # 需要备份的数据库
     DATABASES = list()
 
-
     # 设置发件人和收件人邮箱以及邮件内容
     # 发送备份文件邮箱地址
-    sender_email = '749062870@qq.com'
+    SENDER_EMAIL = None
     # 发送备份文件邮箱授权码
-    sender_password = 'idyvzneokcpjbgah'
+    SENDER_PASSWORD = None
+    # SENDER_PASSWORD = 'idyvzneokcpjbgah'
     # 接收备份文件邮箱
-    receiver_email = 'liqisong2002@gmail.com'
+    RECEIVER_EMAIL = None
     # 发送备份文件邮箱服务商
-    smtp_server = 'smtp.qq.com'
+    SMTP_SERVER = None
     # 发送备份文件邮箱端口
-    smtp_port = 465
-
+    SMTP_PORT = None
 
     # 实例化父类 创建定时任务调度器
     blocking = BlockingScheduler()
@@ -71,13 +70,12 @@ class DatabaseBR:
 
         # 创建一个带附件的邮件对象
         msg = MIMEMultipart()
-        msg['From'] = self.sender_email
-        msg['To'] = self.receiver_email
+        msg['From'] = self.SENDER_EMAIL
+        msg['To'] = self.RECEIVER_EMAIL
         msg['Subject'] = subject
 
         # 添加邮件正文
         msg_text = '备份数据库 ==>  '
-
 
         # 添加附件
         for sql_file in os.listdir(self.BACKUP_PATH):
@@ -92,9 +90,9 @@ class DatabaseBR:
         msg.attach(MIMEText(msg_text, 'plain', 'utf-8'))
 
         # 连接到SMTP服务器并发送邮件
-        with smtplib.SMTP_SSL(self.smtp_server, self.smtp_port) as server:
-            server.login(self.sender_email, self.sender_password)
-            server.sendmail(self.sender_email, self.receiver_email, msg.as_string().encode('utf8'))
+        with smtplib.SMTP_SSL(self.SMTP_SERVER, self.SMTP_PORT) as server:
+            server.login(self.SENDER_EMAIL, self.SENDER_PASSWORD)
+            server.sendmail(self.SENDER_EMAIL, self.RECEIVER_EMAIL, msg.as_string().encode('utf8'))
             server.quit()
         logging.info(f'邮件发送成功')
 
@@ -105,20 +103,25 @@ class DatabaseBR:
         """
 
         # 根据系统生成默认sql保存目录
-        if platform.system().lower() == "windows" :
-            self.BACKUP_PATH = "C:/.db/db_file/"
+        if platform.system().lower() == "windows":
+            self.BACKUP_PATH = os.path.join(f"C:\\Users\\{getpass.getuser()}", "db_file\\")
         else:
             # 如果是mac或者linux就在这里创建默认得保存文件夹  /home/用户/db_file
             self.BACKUP_PATH = os.path.join(f"/home/{getpass.getuser()}", "db_file/")
-            
+
         logging.info(f'当前系统为 {platform.system().lower()}')
         logging.info(f'当前系统登录用户为 {getpass.getuser()}')
 
         databases = list()
 
+        # 配置了字典数据库就必须走字典配置 只有忽略/备份/存储位置不是必须配置到字典 其它值必须走字典
         if all([MYSQL_DICT_PORT, MYSQL_DICT_DB, MYSQL_DICT_PASSWORD, MYSQL_DICT_USERNAME, MYSQL_DICT_HOST]):
+            logging.info(f'读取字典配置开始')
             # 填充数据库配置
             self.fill_db_field()
+            # 填充邮箱配置
+            self.fill_mail_field()
+            logging.info(f'读取字典配置结束')
             # 读取全部待备份数据库名称
             if (len(self.DATABASES) > 0):
                 databases = self.DATABASES
@@ -126,7 +129,10 @@ class DatabaseBR:
                 databases = self.read_all_databases()
             else:
                 databases = self.read_all_databases()
+
+        # 没有配置字典就全部走硬编码 不然代码报错
         else:
+
             # 读取全部待备份数据库名称
             logging.info(f"备份文件路径使用默认生成路径 : {self.BACKUP_PATH}")
             databases = self.read_all_databases()
@@ -201,7 +207,8 @@ class DatabaseBR:
         Args:
             db:要连接的数据库名称
         """
-        conn = pymysql.connect(host=self.MYSQL_HOST, port=self.MYSQL_PORT, user=self.MYSQL_USERNAME, password=self.MYQSL_PASSWORD,
+        conn = pymysql.connect(host=self.MYSQL_HOST, port=self.MYSQL_PORT, user=self.MYSQL_USERNAME,
+                               password=self.MYQSL_PASSWORD,
                                db='mysql')
         return conn
 
@@ -216,13 +223,52 @@ class DatabaseBR:
                                db=MYSQL_DICT_DB)
         return conn
 
+    def fill_mail_field(self):
+        """
+       根据字典编号查出对应的值 并且赋值
+       :return:
+       """
+        try:
+            conn_dict = self.create_dict_mysql_conn()
+            cursor = conn_dict.cursor()
+            # 查询各字段的值
+            cursor.execute("select dictValue from sys_dict where dictCode = 'MYSQL_BACK_UP_DB_SEND_EMAIL'")
+            res = cursor.fetchall()
+            self.SENDER_EMAIL = list({item[0] for item in res})[0]
+            logging.info(f'发送邮件邮箱 {self.SENDER_EMAIL}')
+
+            cursor.execute("select dictValue from sys_dict where dictCode = 'MYSQL_BACK_UP_DB_RECEIVE_EMAIL'")
+            res = cursor.fetchall()
+            self.RECEIVER_EMAIL = list({item[0] for item in res})[0]
+            logging.info(f'接收邮件邮箱 {self.RECEIVER_EMAIL}')
+
+            cursor.execute("select dictValue from sys_dict where dictCode = 'MYSQL_BACK_UP_DB_SEND_EMAIL_AUTHORIZED'")
+            res = cursor.fetchall()
+            self.SENDER_PASSWORD = list({item[0] for item in res})[0]
+            logging.info(f'发送邮件邮箱的授权码 {self.SENDER_PASSWORD}')
+
+            cursor.execute("select dictValue from sys_dict where dictCode = 'MYSQL_BACK_UP_DB_SEND_EMAIL_PORT'")
+            res = cursor.fetchall()
+            self.SMTP_PORT = int(list({item[0] for item in res})[0])
+            logging.info(f'发送邮件邮箱的端口 {self.SMTP_PORT}')
+
+            cursor.execute("select dictValue from sys_dict where dictCode = 'MYSQL_BACK_UP_DB_SEND_EMAIL_SERVER'")
+            res = cursor.fetchall()
+            self.SMTP_SERVER = list({item[0] for item in res})[0]
+            logging.info(f'发送邮件邮箱的服务商 {self.SMTP_SERVER}')
+
+            cursor.close()
+            conn_dict.close()
+
+        except Exception as e:
+            logging.error(f'读取字典配置不全或者网络错误')
+
+
     def fill_db_field(self):
         """
         根据字典编号查出对应的值 并且赋值
         :return:
         """
-        logging.info(f'读取字典配置开始')
-
         try:
             conn_dict = self.create_dict_mysql_conn()
             cursor = conn_dict.cursor()
@@ -231,7 +277,8 @@ class DatabaseBR:
             cursor.execute("select dictValue from sys_dict where dictCode = 'MYSQL_BACK_UP_DB_IGNORE_NAMES'")
             res = cursor.fetchall()
             IGNORE_DATABASES_STRING = list({item[0] for item in res})[0]
-            self.IGNORE_DATABASES = set(IGNORE_DATABASES_STRING.split(':')) if len(IGNORE_DATABASES_STRING) > 0 else self.IGNORE_DATABASES
+            self.IGNORE_DATABASES = set(IGNORE_DATABASES_STRING.split(':')) if len(
+                IGNORE_DATABASES_STRING) > 0 else self.IGNORE_DATABASES
             # information_schema: mysql:performance_schema: sys
             logging.info(f'读取忽略数据库 {self.IGNORE_DATABASES}')
 
@@ -261,19 +308,21 @@ class DatabaseBR:
             self.MYSQL_PORT = int(list({item[0] for item in res})[0])
             logging.info(f'读取数据库端口 {self.MYSQL_PORT}')
 
-
             cursor.execute("select dictValue from sys_dict where dictCode = 'MYSQL_BACK_UP_DB_PATH'")
             res = cursor.fetchall()
-            self.BACKUP_PATH = list({item[0] for item in res})[0] if len(list({item[0] for item in res})[0]) > 0 else self.BACKUP_PATH
-            logging.info(f'读取备份文件存储目录 {self.MYQSL_PASSWORD}')
+            self.BACKUP_PATH = list({item[0] for item in res})[0] if len(
+                list({item[0] for item in res})[0]) > 0 else self.BACKUP_PATH
+            logging.info(f'读取备份文件存储目录 {self.BACKUP_PATH}')
+
+            cursor.close()
+            conn_dict.close()
 
         except Exception as e:
             logging.error(f'读取字典配置不全或者网络错误')
-        logging.info(f'读取字典配置结束')
 
     def con_task(self):
         """执行定时任务的方法"""
-        trigger_min = IntervalTrigger(hours=1)
+        trigger_min = IntervalTrigger(seconds=6)
         self.blocking.add_job(self.backing, trigger=trigger_min, id="backing-task")
         self.blocking.start()
         # self.blocking.pause_job(job_id="backing-task")  # 暂停任务
@@ -281,8 +330,5 @@ class DatabaseBR:
 
 
 if __name__ == '__main__':
-
     dbBack = DatabaseBR()
     dbBack.con_task()
-
-
